2e955f07efe9f1fbc5bf536e8ad0f928121d5b57
Nanachi <dinhvu2509@gmail.com>
2023-08-20 05:27:12 +0700

    update

--- lib/osb/math.rb
+++ lib/osb/math.rb
@@ -2,8 +2,8 @@
 
 module Math
   # Returns whether 2 real numbers are equal within tolerance.
-  # @param [Float] x
-  # @param [Float] y
+  # @param [Numeric] x
+  # @param [Numeric] y
   # @param [Numeric] tolerance
   # @return [Boolean]
   def self.fuzzy_equal(x, y, tolerance = 1e-8)
